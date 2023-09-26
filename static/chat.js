bc9f41998d8cf6598f7f437b2e0cc0b44e146fe6
Madhav Kumar <78339236+Madhav-MKNC@users.noreply.github.com>
2023-09-26 07:08:43 +0530

    Update chat.js

--- static/chat.js
+++ static/chat.js
@@ -1,24 +1,35 @@
+let conversationHistory = [];
+
 function appendMessage(message, fromUser) {
     const chatBox = document.getElementById('chatBox');
     const messageDiv = document.createElement('div');
     messageDiv.className = 'message ' + (fromUser ? 'from-user' : 'from-chatbot');
     messageDiv.innerHTML = `<p>${message}</p>`;
+
     chatBox.appendChild(messageDiv);
     chatBox.scrollTop = chatBox.scrollHeight; // Scroll to bottom
+
+    // Update conversation history
+    conversationHistory.push(
+        `${fromUser ? 'user' : 'assistant'}: ${message}`
+    );
 }
 
-let sendingMessage = false; 
+let sendingMessage = false;
 
 function sendMessage() {
-    if (sendingMessage) return; 
+    if (sendingMessage) return;
 
     const userInput = document.getElementById('userInput').value;
 
     if (userInput.trim() === '') return;
 
-    appendMessage(userInput, true);
+    sendingMessage = true;
 
-    sendingMessage = true; 
+    // Disable the input field while waiting for the response
+    document.getElementById('userInput').disabled = true;
+
+    appendMessage(userInput, true);
 
     // Make an AJAX request to the backend to get chatbot response
     fetch('/get_chat_response', {
@@ -26,22 +37,38 @@ function sendMessage() {
         headers: {
             'Content-Type': 'application/json',
         },
-        body: JSON.stringify({ message: userInput }),
+        body: JSON.stringify({ 
+            message: userInput, 
+            conversationHistory: conversationHistory 
+        }), // Include conversation history
     })
         .then(response => response.json())
         .then(data => {
             const chatbotResponse = data.message;
             appendMessage(chatbotResponse, false);
+
+            // Enable the input field after the response is received
+            document.getElementById('userInput').disabled = false;
+            sendingMessage = false;
+
+            // Focus on the input box after the response is posted
+            document.getElementById('userInput').focus();
         })
         .catch(error => console.error('Error:', error));
 
     // Clear user input field
     document.getElementById('userInput').value = '';
-    sendingMessage = false; 
 }
 
 function handleKeyPress(event) {
     if (event.key === 'Enter') {
         sendMessage();
     }
+}
+
+// Example: Print the entire conversation history (if needed)
+function printConversation() {
+    conversationHistory.forEach(entry => {
+        console.log(`From User: ${entry.fromUser}, Message: ${entry.message}`);
+    });
 }
